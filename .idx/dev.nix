a1c125a59baec33188ee1dcb64ead80ccf2a8783
krishna.p.choudhary.20 <krishna.p.choudhary.20@gmail.com>
2026-01-07 08:03:11 +0000

    feat: done the setup for the prisma and created the sqlite db

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,24 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-23.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
+  channel = "stable-23.11";
+
   packages = [
     pkgs.nodejs_20
+    pkgs.openssl.dev
   ];
-  # Sets environment variables in the workspace
-  env = {};
+
+  env = { };
+
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "google.gemini-cli-vscode-ide-companion"
     ];
+
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
+        npm-install = "npm ci --no-audit --prefer-offline --no-progress";
       };
-      # Runs when a workspace is (re)started
-      onStart= {
+
+      onStart = {
         run-server = "npm run dev";
       };
     };
